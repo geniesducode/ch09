677b0b42bef2c0f9d7b1e6f0c2d9a80ec1fc4d90
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-27 22:13:16 +0200

    07: adapt total points

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render, get_object_or_404, redirect
 from quiz.models import Question
 from quiz.utils import get_rank
@@ -42,9 +43,9 @@ def results(request):
     for question in Question.objects.all():
         answer = request.session['answers'][str(question.id)]
         if question.is_correct(answer):
-            score += 1
+            score += question.points
 
-    score_percentage = int(score / total_questions * 100)
+    score_percentage = score / Question.objects.aggregate(points=Sum('points'))['points'] * 100
     rank = get_rank(score_percentage)
     del request.session['answers']
 
