1ede133aceee973adf22460c1bcc1534df9f9ff1
Sylvain Fankhauser <sylvain.fankhauser@liip.ch>
2018-07-27 19:54:07 +0200

    03: use environment variables for DEBUG and SECRET_KEY

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'PasTrèsSecret!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
+
 
 ALLOWED_HOSTS = []
 
